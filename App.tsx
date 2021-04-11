51459a374e585e3acecbfc39945468b0c7069751
dm9717 <daisuke.masuda97@gmail.com>
2021-04-10 21:50:44 -0700

    Being able to show data stored in Firestore on the screen.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {getShops} from "./src/lib/firebase"
+import {Shop} from "./src/types/shop"
 
 export default function App() {
+  const [shops, setShops] = useState<Shop[]>([])
+
+  // The first arugment is called when the second argument is changed.
+  // But since it is an empty array in this case, the first argument
+  // is called only once when the screen is mounted.
+  useEffect(() => {
+    getFirebaseItems();
+  }, [])
+
+  const getFirebaseItems = async() => {
+    const shops = await getShops()
+    // setShops(shops) is called after snapshot is retrieved, because this is an async function.
+    // If this is not were an async function, setShops(shops) is executed while/before snapshot is retrieved
+    setShops(shops)
+  }
+
+  const shopItems = shops.map((shop, index) => (
+    <View style = {{margin: 10}} key={index.toString()}>
+      <Text>{shop.name}</Text>
+      <Text>{shop.place}</Text>
+    </View>
+  ))
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {shopItems}
     </View>
   );
 }
